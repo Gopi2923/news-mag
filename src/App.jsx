01a2c9ca24cee7c9c500fbb355d903b431746eb1
Gopi2923 <dgopidhikshan@gmail.com>
2024-08-07 10:27:40 +0530

    integrated the news api.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from './Components/Navbar'
+import NewsBoard from './Components/NewsBoard'
 
 const App = () => {
   return (
     <div>
-      
+     <Navbar />
+     <NewsBoard />
     </div>
   )
 }
